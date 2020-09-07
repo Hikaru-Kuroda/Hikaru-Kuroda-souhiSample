39668e1180e19ee7abe2633aa4d859a233e4e0e1
hikaru <hika49050@gmail.com>
2020-09-07 13:11:16 +0900

    first commit

---
+++ soushiSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soushiSample
+//
+//  Created by 黑田光 on 2020/09/07.
+//  Copyright © 2020 Hikaru Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
