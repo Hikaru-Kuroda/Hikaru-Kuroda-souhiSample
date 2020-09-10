b572d3a2636dcaa12c3d95b614a28c67223b7ab2
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2020-09-10 20:34:13 +0900

    test

--- soushiSample/ViewController.swift
+++ soushiSample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("test")
-        print("test2")
+        print("test")
     }
 
 
