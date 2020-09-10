da8e97b2f79023465751684005115b7a7b340fbc
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2020-09-10 21:11:29 +0900

    課題1

---
+++ soushiSample/TaskFile/Tasi1.swift
@@ -0,0 +1,21 @@
+//
+//  Tasi1.swift
+//  soushiSample
+//
+//  Created by TanakaSoushi on 2020/09/10.
+//  Copyright © 2020 Hikaru Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class Tasi1: NSObject {
+    //ひかるさんは少しペース早めでいきます！
+    //課題１
+    //コードのみでUILabelとUIButtonを配置してください。
+    //シュミレーターに表示されたら成功です
+    //課題２
+    //コードのみでUIViewを配置し、角丸にして影をつけてください。
+    //ViewControllerの背景はUIColor.systemGray6にしてください
+    //追記
+    //配置等は自由ですが、なるべくきれいに見えるように実装してください！
+}
