177eace4b476f5b6f482898dc0156af2f6ef3a80
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2020-09-12 20:18:46 +0900

    課題4

--- soushiSample/TaskFile/Task3.swift
+++ soushiSample/TaskFile/Task3.swift
@@ -1,32 +1,75 @@
 //
-//  Task3.swift
+//  ViewController.swift
 //  soushiSample
 //
-//  Created by TanakaSoushi on 2020/09/11.
+//  Created by 黑田光 on 2020/09/07.
 //  Copyright © 2020 Hikaru Kuroda. All rights reserved.
 //
 
 import UIKit
 
-class Task3: UIViewController {
-
+class ViewController3: UIViewController {
+    
+    private let screenHeight = UIScreen.main.bounds.height
+    
+    private let firstView = UIView()
+    private let secondView = UIView()
+    private let thirdView = UIView()
+    private let colors = Colors()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUpContents()
     }
-    //課題3
-    //http://photoshopvip.net/95427
-    //https://liginc.co.jp/399974
-    //以上の２サイトから、colorZillaなど画面内の色をカラーコードで取得するツールを使って
-    //お気に入りの配色を見つけて
-    //structにまとめてください(Colorファイルとして別ファイルにまとめる)
-    //structの数は幾つでもokです
-    //struct Colors {
-    //  let blue:UIColor = .init(displayP3Red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
-    //}
-    //これを利用するにはColors()インスタンスを作成し、colors変数などに代入
-    //colors.blue などとして利用する
-    //extensionに記述しない理由は、自分が組んだ色しか表示されないことを保証するためです。
+    
+    private func setUpContents() {
+        self.view.backgroundColor = colors.cyan
+    
+        setUpLabel(uiView: firstView, title: "This is FirstView")
+        setUpButton(uiView: firstView, title: "Button")
+        setUpView(uiView: firstView, topAnchor: view.topAnchor, topConstrait: screenHeight * 0.15)
+        
+        setUpLabel(uiView: secondView, title: "This is SecondView")
+        setUpButton(uiView: secondView, title: "Button")
+        setUpView(uiView: secondView, topAnchor: firstView.bottomAnchor, topConstrait: 20)
+        
+        setUpLabel(uiView: thirdView, title: "This is ThirdView")
+        setUpButton(uiView: thirdView, title: "Button")
+        setUpView(uiView: thirdView, topAnchor: secondView.bottomAnchor, topConstrait: 20)
+    }
+    
+    private func setUpView(uiView: UIView, topAnchor: NSLayoutAnchor<NSLayoutYAxisAnchor>,topConstrait: CGFloat) {
+        uiView.backgroundColor = colors.lightGreen
+        uiView.layer.cornerRadius = 20
+        uiView.layer.shadowColor = .init(srgbRed: 0, green: 0, blue: 0, alpha: 0.2)
+        self.view.addSubview(uiView)
+        uiView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        uiView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+        uiView.topAnchor.constraint(equalTo: topAnchor, constant: topConstrait).isActive = true
+        uiView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+    }
+
+    private func setUpLabel(uiView: UIView, title: String) {
+        let label = UILabel()
+        label.text = title
+        label.textColor = colors.darkGray
+        uiView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.bottomAnchor.constraint(equalTo: uiView.centerYAnchor, constant: -20).isActive = true
+        label.centerXAnchor.constraint(equalToSystemSpacingAfter: uiView.centerXAnchor, multiplier: 0).isActive = true
+    }
+    
+    private func setUpButton(uiView: UIView, title: String) {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(colors.darkGray, for: .normal)
+        uiView.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: uiView.centerYAnchor, constant: 20).isActive = true
+        button.centerXAnchor.constraint(equalToSystemSpacingAfter: uiView.centerXAnchor, multiplier: 0).isActive = true
+    }
 
 }
+
---
+++ soushiSample/TaskFile/Task4.swift
@@ -0,0 +1,33 @@
+//
+//  Task4.swift
+//  soushiSample
+//
+//  Created by TanakaSoushi on 2020/09/12.
+//  Copyright © 2020 Hikaru Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class Task4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    課題4
+     ①課題３で作成したColorsStructで、紹介したサイトのどの部分を参考にしたか教えてください！
+     (ViewControllerの最初にコメントアウトで書いてください。個人的に知りたいです笑)
+     ②ScrollViewをストーリーボードを用いて良いので設置してください
+     レイアウト対策としてよく使われるのがScrollViewです。設定欄、サイドメニュー、説明欄、自分のコロナアプリの
+     健康診断の部分もそうですが、ScrollViewを用いてあらかじめ縦長に対応しておくと、縦の幅の小さな端末を
+     気にすることなくコンテンツを作成することができます。
+     今回はこのトップ画面のコンテンツを6セットにして、ScrollViewの上に配置してください。
+     ※ScrollViewの利用にはコツがあるのでストーリーボードを使ってもらったほうが良いかと思います。
+     ③Colors構造体を使って自分なりの配色をしてください。グラデーションを利用すると尚良いかと思います！
+    */
+
+}
