4564a8d7e83512128c13d8dab7e6607731b6f416
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2020-09-15 22:23:53 +0900

    Fix1

---
+++ soushiSample/TaskFile/Fix6.swift
@@ -0,0 +1,44 @@
+//
+//  Fix6.swift
+//  soushiSample
+//
+//  Created by TanakaSoushi on 2020/09/15.
+//  Copyright © 2020 Hikaru Kuroda. All rights reserved.
+//
+
+import UIKit
+
+class Fix6: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    Fix案件です。
+     ・小コンテンツのUIViewの背景は白で、scrollView部分の背景を好きな色にして
+     　それに合わせてヘッダーとフッターの色を合わせてみてください。
+       (全体がモノトーンになる配色以外にしてみてください)
+     ・以下をpodinstallしてください。
+     pod 'RealmSwift'
+     pod 'Alamofire', '~> 5.2'
+     pod 'SwiftyJSON'
+     pod 'FSCalendar'
+     pod 'CalculateCalendarLogic'
+     pod 'Charts'
+     pod 'KRProgressHUD'
+     pod 'Firebase/Analytics'
+     pod 'Firebase/Auth'
+     pod 'Firebase/Core'
+     pod 'Firebase/Firestore'
+     pod 'FirebaseFirestoreSwift'
+     pod 'MessageKit'
+     pod 'MessageInputBar'
+     ・インスタンスは使いまわせるのであれば極力使いまわしたいので、Colors構造体のインスタンスを
+     　グローバル変数に代入して再利用してください。
+    */
+
+}
