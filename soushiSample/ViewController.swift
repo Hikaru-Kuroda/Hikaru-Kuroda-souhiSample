0bf769bdae9fde49bfc73c704f641e849faefbe7
hikaru <hika49050@gmail.com>
2020-09-18 07:57:41 +0900

    Task7 done

--- soushiSample/ViewController.swift
+++ soushiSample/ViewController.swift
@@ -139,6 +139,46 @@ class ViewController: UIViewController {
         button.topAnchor.constraint(equalTo: uiView.centerYAnchor, constant: 10).isActive = true
         button.centerXAnchor.constraint(equalToSystemSpacingAfter: uiView.centerXAnchor, multiplier: 0).isActive = true
     }
-
 }
 
+/*
+ https://covid19-japan-web-api.now.sh/api//v1/prefectures
+ {
+     id: 1,
+     name_ja: "北海道",
+     name_en: "Hokkaido",
+     lat: 43.46722222,
+     lng: 142.8277778,
+     population: 5248552,
+     last_updated: {
+     cases_date: 20200917,
+     deaths_date: 20200917,
+     pcr_date: 20200917,
+     hospitalize_date: 20200917,
+     severe_date: 20200917,
+     discharge_date: 20200917,
+     symptom_confirming_date: 20200917
+     },
+     cases: 1892,
+     deaths: 106,
+     pcr: 50527,
+     hospitalize: 62,
+     severe: 2,
+     discharge: 1724,
+     symptom_confirming: 0
+},
+ */
+
+/*
+ https://covid19-japan-web-api.now.sh/api//v1/total
+ {
+     date: 20200917,
+     pcr: 1789960,
+     hospitalize: 6267,
+     positive: 77721,
+     severe: 167,
+     discharge: 69882,
+     death: 1486,
+     symptom_confirming: 50
+ }
+ */
