010b93d505e36524070e18f1d082d25bf69382c3
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2020-09-25 00:42:30 +0900

    Fix10

---
+++ soushiSample/TaskFile/Fix10.swift
@@ -0,0 +1,13 @@
+/*
+ Fixになります。
+ getTotal()
+ getPrefecture()
+ の下の行で、グローバル変数のtotalResult、totalPrefectureにアクセスしたのですが、
+ nilになりました。何か処理が足りないようです。
+ 非同期処理は、リクエストを投げる関数でエスケープ処理をしてあげないと
+ そのデータを外部に持ち越すことができません。
+ 
+ getTotal()
+ getPrefecture()
+ の下の行でtotalResult、totalPrefectureにアクセスできるよう、修正してみてください！
+ **/
